473cfc1c77a6d41eb69a301eebb1bb7685885623
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-03-24 17:46:04 +0100

    Update auto-installer-qtile.sh

--- auto-installer-qtile.sh
+++ auto-installer-qtile.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-echo "¡Bienvenido al instalador de qtile!"
+# Instalar qtile y sus dependencias
+sudo pacman -S qtile xorg xorg-xinit python-pip
 
-# Preguntar al usuario si desea instalar un gestor de archivos
+# Configurar qtile
+echo "Creando archivo de configuración de qtile..."
+mkdir -p ~/.config/qtile
+cp /usr/share/doc/qtile/default_config.py ~/.config/qtile/config.py
+
+# Configurar la tecla mod de qtile
+read -p "¿Desea configurar la tecla mod de qtile? (y/n) " configure_mod_key
+
+if [ "$configure_mod_key" = "y" ]; then
+    echo "Seleccione una tecla mod:"
+    echo "1. Super (tecla Windows)"
+    echo "2. Alt"
+    read -p "Opción: " mod_key_option
+
+    if [ "$mod_key_option" = "1" ]; then
+        echo "Configurando tecla mod a Super..."
+        sed -i 's/mod = "mod4"/mod = "mod4"/g' ~/.config/qtile/config.py
+    elif [ "$mod_key_option" = "2" ]; then
+        echo "Configurando tecla mod a Alt..."
+        sed -i 's/mod = "mod4"/mod = "mod1"/g' ~/.config/qtile/config.py
+    else
+        echo "Opción inválida. La tecla mod no se configurará."
+    fi
+fi
+
+# Instalar el gestor de archivos
 read -p "¿Desea instalar un gestor de archivos? (y/n) " install_filemanager
 
 if [ "$install_filemanager" = "y" ]; then
-    # Instalar el gestor de archivos seleccionado por el usuario
     echo "Seleccione un gestor de archivos para instalar:"
     echo "1. Thunar"
     echo "2. Nautilus"
@@ -23,39 +48,6 @@ if [ "$install_filemanager" = "y" ]; then
     fi
 fi
 
-# Preguntar al usuario si desea configurar qtile
-read -p "¿Desea configurar qtile? (y/n) " configure_qtile
-
-if [ "$configure_qtile" = "y" ]; then
-    # Crear el archivo de configuración de qtile
-    echo "Creando archivo de configuración de qtile..."
-    mkdir -p ~/.config/qtile
-    cp /usr/share/doc/qtile/default_config.py ~/.config/qtile/config.py
-
-    # Preguntar al usuario si desea configurar la tecla mod de qtile
-    read -p "¿Desea configurar la tecla mod de qtile? (y/n) " configure_mod_key
-
-    if [ "$configure_mod_key" = "y" ]; then
-        # Configurar la tecla mod de qtile
-        echo "Seleccione una tecla mod:"
-        echo "1. Super (tecla Windows)"
-        echo "2. Alt"
-        read -p "Opción: " mod_key_option
-
-        if [ "$mod_key_option" = "1" ]; then
-            echo "Configurando tecla mod a Super..."
-            sed -i 's/mod = "mod4"/mod = "mod4"/g' ~/.config/qtile/config.py
-        elif [ "$mod_key_option" = "2" ]; then
-            echo "Configurando tecla mod a Alt..."
-            sed -i 's/mod = "mod4"/mod = "mod1"/g' ~/.config/qtile/config.py
-        else
-            echo "Opción inválida. La tecla mod no se configurará."
-        fi
-    fi
-
-    echo "qtile ha sido instalado y configurado correctamente."
-else
-    echo "qtile ha sido instalado correctamente."
-fi
-
-echo "¡Gracias por utilizar este script! Si desea configurar qtile en el futuro, ejecute el comando 'qtile config'."
+# Finalizar
+echo "¡La instalación de qtile ha finalizado! Para iniciar qtile, ejecute el comando 'startx' o 'qtile'." 
+echo "Para modificar la configuración de qtile en el futuro, edite el archivo ~/.config/qtile/config.py."
