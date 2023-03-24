1bc61eb4f4eb552b713b37a4dccc81a39fe14ee8
Ayoub <98343832+ProyetosAyoub@users.noreply.github.com>
2023-03-24 21:47:44 +0100

    Update auto-installer-qtile.sh

--- auto-installer-qtile.sh
+++ auto-installer-qtile.sh
@@ -1,53 +1,25 @@
 #!/bin/bash
 
-# Instalar qtile y sus dependencias
-sudo pacman -S qtile xorg xorg-xinit python-pip
+# Instalar dependencias
+sudo apt-get update
+sudo apt-get install -y git python-pip python-xcffib libcairo2-dev libpango1.0-dev libxcb-ewmh-dev libxcb-util0-dev libxcb1-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libxcb-xrm-dev python-dbus python-gobject python-xlib python3-thunar fish
 
-# Configurar qtile
-echo "Creando archivo de configuración de qtile..."
-mkdir -p ~/.config/qtile
-cp /usr/share/doc/qtile/default_config.py ~/.config/qtile/config.py
+# Instalar QTile
+pip install xcffib
+pip install --no-cache-dir cairocffi
+pip install qtile
 
-# Configurar la tecla mod de qtile
-read -p "¿Desea configurar la tecla mod de qtile? (y/n) " configure_mod_key
+# Clonar el gestor de ventana styling
+git clone https://github.com/antoniosarosi/wal-qtile.git ~/.config/qtile
 
-if [ "$configure_mod_key" = "y" ]; then
-    echo "Seleccione una tecla mod:"
-    echo "1. Super (tecla Windows)"
-    echo "2. Alt"
-    read -p "Opción: " mod_key_option
+# Copiar el archivo de configuración de shortcuts
+wget https://raw.githubusercontent.com/qtile/qtile/master/libqtile/resources/default_config.py -O ~/.config/qtile/config.py
 
-    if [ "$mod_key_option" = "1" ]; then
-        echo "Configurando tecla mod a Super..."
-        sed -i 's/mod = "mod4"/mod = "mod4"/g' ~/.config/qtile/config.py
-    elif [ "$mod_key_option" = "2" ]; then
-        echo "Configurando tecla mod a Alt..."
-        sed -i 's/mod = "mod4"/mod = "mod1"/g' ~/.config/qtile/config.py
-    else
-        echo "Opción inválida. La tecla mod no se configurará."
-    fi
-fi
+# Configurar Thunar como editor de texto predeterminado
+xdg-mime default Thunar.desktop text/plain
 
-# Instalar el gestor de archivos
-read -p "¿Desea instalar un gestor de archivos? (y/n) " install_filemanager
+# Configurar Fish como shell predeterminado
+chsh -s /usr/bin/fish
 
-if [ "$install_filemanager" = "y" ]; then
-    echo "Seleccione un gestor de archivos para instalar:"
-    echo "1. Thunar"
-    echo "2. Nautilus"
-    read -p "Opción: " filemanager_option
-
-    if [ "$filemanager_option" = "1" ]; then
-        echo "Instalando Thunar..."
-        sudo pacman -S thunar
-    elif [ "$filemanager_option" = "2" ]; then
-        echo "Instalando Nautilus..."
-        sudo pacman -S nautilus
-    else
-        echo "Opción inválida. No se instalará ningún gestor de archivos."
-    fi
-fi
-
-# Finalizar
-echo "¡La instalación de qtile ha finalizado! Para iniciar qtile, ejecute el comando 'startx' o 'qtile'." 
-echo "Para modificar la configuración de qtile en el futuro, edite el archivo ~/.config/qtile/config.py."
+# Mensaje de finalización y reinicio del sistema
+echo "¡Instalación completada! Por favor, reinicie el sistema para que los cambios tengan efecto."
